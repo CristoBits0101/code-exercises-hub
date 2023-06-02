ea7445ca8e31c81e8dd488ca575576edc38883df
Cristo Rubén Pérez Suárez <94800536+CristoBits0101@users.noreply.github.com>
2023-06-02 10:57:46 +0100

    Add files via upload

---
+++ JavaScript/07_Loops/Object.Keys().js
@@ -0,0 +1,9 @@
+const obj = {
+    name: 'John',
+    age: 30,
+    profession: 'Developer'
+};
+  
+const keys = Object.keys(obj);
+
+console.log(keys); // Array con todas las claves del objeto.
---
+++ JavaScript/07_Loops/Object.Values().js
@@ -0,0 +1,9 @@
+const obj = {
+    name: 'John',
+    age: 30,
+    profession: 'Developer'
+};
+
+const values = Object.values(obj);
+
+console.log(values); // Array con todos los valores del objeto.
